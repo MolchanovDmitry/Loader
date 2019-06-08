39ed5a536f7ad7e7af6dc4fbc7dce50c02c74605
d.molchanov <penopllast@yandex.ru>
2019-06-08 19:17:52 +0300

    Статус загрузки на экране

--- app/src/main/java/com/penopllast/loader/MainActivity.java
+++ app/src/main/java/com/penopllast/loader/MainActivity.java
@@ -2,6 +2,9 @@ package com.penopllast.loader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.concurrent.ExecutionException;
 
@@ -12,14 +15,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ILoader loader = new Loader();
-        Promise<Result<String>> a = loader.load("https://www.youjtube.com");
-        try {
-            System.out.println(a.get().getErrorMessage());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
+        String[] urls = new String[]{
+                "https://yandex.ru",
+                "https://yan111dex.ru",
+                "h111ttps://yandex.ru",
+                ""
+        };
+        for (String url : urls) {
+            loadUrls(url);
         }
+    }
+
+    private void loadUrls(String url) {
+
+        ILoader loader = new Loader();
+        Promise<Result<String>> promise = loader.load(url);
+
+        Result<String> a = null;
+        String exceptionMessage = "";
+        try {
+            a = promise.get();
+        } catch (InterruptedException | ExecutionException e) {
+            exceptionMessage = e.getMessage();
+        }
+
+        String status = a == null
+                ? exceptionMessage : a.isSuccess()
+                ? "success" : a.getErrorMessage();
+        addRow("url = " + url + "; status = " + status);
+    }
+
+    private void addRow(String text) {
+        LinearLayout layout = findViewById(R.id.root);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        TextView textView = new TextView(this);
+        textView.setText(text);
+        layout.addView(textView, params);
     }
 }
